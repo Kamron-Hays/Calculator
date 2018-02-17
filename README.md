8dfb4de3c5352e1ca9a49a6ad5a6d99e26ea107b
Kamron Hays <kamron.hays@live.com>
2018-02-17 09:26:09 -0500

    Added keyboard support

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator
+
+Part of the Odin Project [curriculum](http://www.theodinproject.com/courses/web-development-101/). Uses Javascript (and JQuery), HTML, and CSS to implement a very basic calculator. Keyboard support is included. Use the 'c' key to clear, and the 'd' key to delete/backspace.
+
+Check out my published version [here](https://kamron-hays.github.io/Calculator/).
--- calculate.js
+++ calculate.js
@@ -1,3 +1,7 @@
+var firstNumber = "";
+var secondNumber = "";
+var operator = "";
+
 function add(num1, num2)
 {
     return (parseFloat(num1) + parseFloat(num2)).toString();
@@ -33,12 +37,91 @@ function operate(num1, num2, op)
     return result;
 }
 
-$(document).ready(function()
+function handleNumeric(num)
 {
-    let firstNumber = "";
-    let secondNumber = "";
-    let operator = "";
+    if ( operator == "" )
+    {
+        firstNumber += num;
+        $('#result').text(firstNumber);
+    }
+    else
+    {
+        secondNumber += num;
+        $('#result').text(secondNumber);
+    }
+}
+
+function handleOperator(op)
+{
+    if ( firstNumber != "" && secondNumber != "" )
+    {
+        // perform the operation
+        firstNumber = operate(firstNumber, secondNumber, operator);
+        $('#result').text(firstNumber);
+    }
+    else if ( firstNumber == "" )
+    {
+        firstNumber =  $('#result').text();
+    }
     
+    operator = op;
+    secondNumber = "";
+}
+
+function handleEquals()
+{
+    if ( operator != "")
+    {
+        if ( firstNumber == "" )
+        {
+            firstNumber =  $('#result').text();
+        }
+
+        if ( secondNumber == "" )
+        {
+            secondNumber = firstNumber;
+        }
+
+        // perform the operation
+        firstNumber = operate(firstNumber, secondNumber, operator);
+        $('#result').text(firstNumber);
+        firstNumber = "";
+    }
+}
+
+function handleClear()
+{
+    $('#result').text(0);
+    firstNumber = "";
+    secondNumber = ""
+    operator = "";
+}
+
+function handleBackspace()
+{
+    let result = $('#result').text();
+    
+    if ( result.length > 1 )
+    {
+        $('#result').text(result.slice(0,-1));
+    }
+    else if ( result != "0" )
+    {
+        $('#result').text(0);
+    }
+    
+    if ( operator == "" )
+    {
+        firstNumber = $('#result').text();
+    }
+    else
+    {
+        secondNumber = $('#result').text();
+    }
+}
+
+$(document).ready(function()
+{    
     $('#result').text(0);
 
     $('button').on('click', function()
@@ -49,82 +132,56 @@ $(document).ready(function()
           case '-':
           case '*':
           case '/':
-            if ( firstNumber != "" && secondNumber != "" )
-            {
-                // perform the operation
-                firstNumber = operate(firstNumber, secondNumber, operator);
-                $('#result').text(firstNumber);
-                operator = this.name;
-            }
-            else if ( firstNumber == "" )
-            {
-                firstNumber =  $('#result').text();
-            }
-            
-            operator = this.name;
-            secondNumber = "";
+            handleOperator(this.name);
             break;
 
           case '=':
-            if ( operator != "")
-            {
-                if ( firstNumber == "" )
-                {
-                    firstNumber =  $('#result').text();
-                }
-
-                if ( secondNumber == "" )
-                {
-                    secondNumber = firstNumber;
-                }
-
-                // perform the operation
-                firstNumber = operate(firstNumber, secondNumber, operator);
-                $('#result').text(firstNumber);
-                firstNumber = "";
-            }
+            handleEquals();
             break;
 
           case 'Backspace':
-            let result = $('#result').text();
-            
-            if ( result.length > 1 )
-            {
-                $('#result').text(result.slice(0,-1));
-            }
-            else if ( result != "0" )
-            {
-                $('#result').text(0);
-            }
-            
-            if ( operator == "" )
-            {
-                firstNumber = $('#result').text();
-            }
-            else
-            {
-                secondNumber = $('#result').text();
-            }
+            handleBackspace();
             break;
 
           case 'C':
-            $('#result').text(0);
-            firstNumber = "";
-            secondNumber = ""
-            operator = "";
+            handleClear();
             break;
 
           default:
-            if ( operator == "" )
-            {
-                firstNumber += this.name;
-                $('#result').text(firstNumber);
-            }
-            else
-            {
-                secondNumber += this.name;
-                $('#result').text(secondNumber);
-            }
+            handleNumeric(this.name);
+            break;
+        }
+    });
+    
+    $(document).keypress(function(event)
+    {
+        switch ( event.which )
+        {
+          case 13: handleEquals(); break;
+          case 42: handleOperator('*'); break;
+          case 43: handleOperator('+'); break;
+          case 45: handleOperator('-'); break;
+          case 47: handleOperator('/'); break;
+          case 49: handleNumeric('1'); break;
+          case 50: handleNumeric('2'); break;
+          case 51: handleNumeric('3'); break;
+          case 52: handleNumeric('4'); break;
+          case 53: handleNumeric('5'); break;
+          case 54: handleNumeric('6'); break;
+          case 55: handleNumeric('7'); break;
+          case 56: handleNumeric('8'); break;
+          case 57: handleNumeric('9'); break;
+          case 46: handleNumeric('.'); break;
+          case 99: // c
+          case 67: // C
+            handleClear();
+            break;
+          case 100: // d
+          case 100: // D
+            handleBackspace();
+            break;
+          default:
+            alert('Handler for .keypress() called. - ' + event.which);
             break;
         }
     });
